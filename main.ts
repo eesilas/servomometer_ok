0fb9f3a9016c3bc818117ff1dbb9beff35d00b1e
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-04 15:01:20 +0800

    Display temperature on OLED micro:IoT

--- main.ts
+++ main.ts
@@ -12,6 +12,7 @@ basic.forever(function () {
         temp = 40
     } else {
         basic.showString("" + (input.temperature()))
+        microIoT.microIoT_showUserNumber(1, input.temperature())
         pins.servoWritePin(AnalogPin.P0, pins.map(
         temp,
         20,
